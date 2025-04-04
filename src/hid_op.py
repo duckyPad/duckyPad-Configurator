dc3124a40071fbe86bcaa54ea67ce9486c8950fb
dekuNukem <dekunukem@gmail.com>
2025-04-04 19:02:52 +0100

    seperated out hid common functions

--- src/hid_op.py
+++ src/hid_op.py
@@ -6,39 +6,10 @@ import psutil
 import my_compare
 from shared import *
 from pathlib import Path
+from hid_common import *
 
 if 'win32' in sys.platform:
     import win32api
-
-dp20_pid = 0xd11c
-dpp_pid = 0xd11d
-all_dp_pids = [dp20_pid, dpp_pid]
-
-def is_duckypad_pid(this_pid):
-    return this_pid in all_dp_pids
-
-def get_duckypad_path():
-    dp_path_list = set()
-    if 'win32' in sys.platform:
-        for device_dict in hid.enumerate():
-            if device_dict['vendor_id'] == 0x0483 and \
-            is_duckypad_pid(device_dict['product_id']) and \
-            device_dict['usage'] == 58:
-                dp_path_list.add(device_dict['path'])
-    else:
-        for device_dict in hid.enumerate():
-            if device_dict['vendor_id'] == 0x0483 and \
-            is_duckypad_pid(device_dict['product_id']):
-                dp_path_list.add(device_dict['path'])
-    return list(dp_path_list)
-
-PC_TO_DUCKYPAD_HID_BUF_SIZE = 64
-DUCKYPAD_TO_PC_HID_BUF_SIZE = 64
-
-HID_RESPONSE_OK = 0
-HID_RESPONSE_ERROR = 1
-HID_RESPONSE_BUSY = 2
-HID_RESPONSE_EOF = 3
 
 def duckypad_hid_sw_reset(dp_dict, reboot_into_usb_msc_mode=False):
     print("duckypad_hid_sw_reset", dp_dict)
@@ -101,44 +72,6 @@ def eject_drive(vol_str):
     else:
         time.sleep(1) # well, good enough for windows
 
-def make_dp_info_dict(hid_msg, hid_path):
-    this_dict = {}
-    this_dict['fw_version'] = f"{hid_msg[3]}.{hid_msg[4]}.{hid_msg[5]}"
-    this_dict['dp_model'] = hid_msg[6]
-    serial_number_uint32_t = int.from_bytes(hid_msg[7:11], byteorder='big')
-    this_dict['serial'] = f'{serial_number_uint32_t:08X}'.upper()
-    this_dict['hid_path'] = hid_path
-    this_dict['hid_msg'] = hid_msg
-    return this_dict
-
-def get_all_dp_info(dp_path_list):
-    dp_info_list = []
-    pc_to_duckypad_buf = [0] * PC_TO_DUCKYPAD_HID_BUF_SIZE
-    pc_to_duckypad_buf[0] = 5   # HID Usage ID, always 5
-    for this_path in dp_path_list:
-        print(this_path)
-        myh = hid.device()
-        myh.open_path(this_path)
-        myh.write(pc_to_duckypad_buf)
-        result = myh.read(DUCKYPAD_TO_PC_HID_BUF_SIZE)
-        myh.close()
-        print(result)
-        if result[2] != HID_RESPONSE_OK:
-            continue
-        this_dict = make_dp_info_dict(result, this_path)
-        dp_info_list.append(this_dict)
-    return dp_info_list
-
-def scan_duckypads():
-    all_dp_paths = get_duckypad_path()
-    if len(all_dp_paths) == 0:
-        return []
-    try:
-        dp_info_list = get_all_dp_info(all_dp_paths)
-    except Exception:
-        return None
-    return dp_info_list
-
 def make_hid_file_path(file_op):
     result = os.path.join(file_op.local_dir, file_op.source_path)
     result = result.lstrip('\\/')
@@ -154,11 +87,6 @@ def write_str_into_buf(text, buf):
 def write_bytes_into_buf(bin_buf, buf):
     for index, value in enumerate(bin_buf):
         buf[3+index] = value
-
-def get_empty_pc_to_duckypad_buf():
-    ptd_buf = [0] * PC_TO_DUCKYPAD_HID_BUF_SIZE
-    ptd_buf[0] = 5   # HID Usage ID
-    return ptd_buf
 
 def split_file_to_chunks(path, chunk_size=60):
     path = Path(path)
@@ -188,12 +116,6 @@ def hid_write_file(file_op, hid_obj):
     pc_to_duckypad_buf = get_empty_pc_to_duckypad_buf()
     pc_to_duckypad_buf[2] = HID_COMMAND_CLOSE_FILE
     hid_txrx(pc_to_duckypad_buf, hid_obj)
-
-def hid_txrx(buf_64b, hid_obj):
-    # print("\n\nSending to duckyPad:\n", buf_64b)
-    hid_obj.write(buf_64b)
-    duckypad_to_pc_buf = hid_obj.read(DUCKYPAD_TO_PC_HID_BUF_SIZE)
-    # print("\nduckyPad response:\n", duckypad_to_pc_buf)
 
 def do_hid_fileop(this_op, hid_obj):
     pc_to_duckypad_buf = get_empty_pc_to_duckypad_buf()
